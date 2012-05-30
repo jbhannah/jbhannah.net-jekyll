affca319dd5d30940ed924d96bbdd0a16b015f52
Jesse B. Hannah <jesse@jbhannah.net>
2012-05-29 22:45:56 -0700

    Better external link detection
    Separate detection of external links and application of rel="external"
    from application of target="_blank" and Google Analytics event tracking.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 $(function() {
-  $('a[href^="http"]').attr({
-    rel:    'external'
-   ,target: '_blank'
-  }).click(function() {
+  $('a[href^=http]').each(function() {
+    if (this.href.indexOf(location.hostname) == -1) {
+      var rel = $(this).attr('rel');
+      if (rel != undefined) {
+        rel += ' ';
+      } else {
+        rel = '';
+      }
+      $(this).attr('rel', rel + 'external');
+    }
+  });
+
+  $('a[rel~=external]').attr('target', '_blank').click(function() {
     _gaq.push(['_trackEvent', 'Outbound Links', $(this).href, $(this).text]);
   });
 });
