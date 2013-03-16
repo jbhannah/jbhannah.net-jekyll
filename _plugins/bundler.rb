f3df49215e1d11696d7384b6e3969aae24d9341f
Jesse B. Hannah <jesse@jbhannah.net>
2013-03-15 18:39:36 -0700

    Require gems using Bundler instead of manually

---
+++ _plugins/bundler.rb
@@ -0,0 +1,4 @@
+require "rubygems"
+require "bundler/setup"
+Bundler.require(:default)
+require "jekyll-assets/compass"
--- _plugins/ext.rb
+++
@@ -1,6 +0,0 @@
-require "jekyll-assets"
-require "compass"
-require "zurb-foundation"
-require "jekyll-assets/compass"
-require "uglifier"
-require "coffee_script"
